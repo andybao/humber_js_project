7e53f958c0bc87915a174501b8d6851de1b35c76
tyzia <tyziaa@gmail.com>
2017-12-05 23:36:28 -0500

    added google maps and flex to footer

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
-// check if connected
-//alert("Hi there!");
+// Get Google map
+
+function initMap() {
+	var uluru = {lat: 43.729206000, lng: -79.604948700};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
+
+
+
+
+// function initMap() {
+//         var uluru = {lat: -25.363, lng: 131.044};
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 4,
+//           center: uluru
+//         });
+//         var marker = new google.maps.Marker({
+//           position: uluru,
+//           map: map
+//         });
+//       }
