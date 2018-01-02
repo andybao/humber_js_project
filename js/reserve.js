0e7f5dbe8973f32473accfbf491e2c0c9c93c599
gurinder <guribanwait07@gmail.com>
2018-01-01 21:09:14 -0500

    added

--- js/reserve.js
+++ js/reserve.js
@@ -1,31 +1,91 @@
- window.onload = ready;
+window.onload = ready;
 function ready()
 {
-	
-	  var formHandle = document.forms[0];
-	//on submit form
-	formHandle.onsubmit = processForm;
-	function processForm()
+	// set min date value
+	 var today = new Date();
+	var day = today.getDate();
+	var mon = today.getMonth()+1;
+	var year = today.getFullYear();
+	if(day<10)
 	{
-		var today = new Date();
-		//get date, time and party size
-		var d = document.forms[0].userdate.value;
-		var t =  document.forms[0].usertime.value;
-		var ps = document.forms[0].party.value;
-		
-		//hide main-para and form on submit
-		document.getElementById('main-p').style.display = "none";
-		document.forms[0].style.display = "none";
-		var m = document.getElementById('message');
-		m.style.color = "#824328";
-		m.style.fontSize = "20px";
-		
-		m.innerHTML = "Table is reserved. <br>Reservation details are:<br> Date: " + d + "<br> Time: " + t + "<br> Party Size: " + ps + ".";
-		
-		
+		day = '0'+ day;
+	}
+	if(mon<10)
+	{
+		mon='0' + mon;
+	}
+	today = year + '-' + mon + '-' + day;
+	document.getElementById("form-date").setAttribute("min",today);
+	 
+	
+	//set maxdate value
+	 var enddate =  new Date();
+	enddate.setDate(new Date().getDate()+30);//30 days range for reservation
+	
+   
+	 var endday = enddate.getDate();
+	var endmon = enddate.getMonth()+1;
+	var endyear = enddate.getFullYear();
+	if(endday <10)
+	{ endday = '0' + endday;}
+	if(endmon <10)
+	{ endmon = '0' + endmon; }
+	
+	enddate = endyear + '-' + endmon + '-' + endday; 
+	
+	document.getElementById("form-date").setAttribute("max",enddate);
+		  
+  var formHandle = document.forms[0];
+  //on submit form
+  formHandle.onsubmit = processForm;
+  function processForm()
+	{
+	  //get date, time and party size
+	  var d = document.getElementById("form-date").value;
+	  var t =  document.getElementById("form-time").value;
+	  var ps = document.getElementById("party-size");
+	  //validate date
+	  console.log(d);
+	  var days = d.getDate();
+	  var month = d.getMonth();
+	  var years = d.getFullYear();
+	  
+	 // console.log(month);
+	  if(d.getDate() === "")// || month.value === "" || years.value === "")
+	  {
+		  alert("Select Date");
+		  t.focus();
+		  return false;
+	  } 
+	   
+	  //validate time
+	  if(t.value === "" || t.indexOf(":")<0)
+	  {
+		  alert("Select Time");
+		  t.focus();
+		  return false;
+	  } 
+	   
+	 
+	  
+	  
+		//validate party size
+	  if(ps.value==="")
+		{
+			alert("choose party size");
+			ps.focus();
+			return false;
+		}		
+	  //hide main-para and form on submit
+	  document.getElementById('main-p').style.display = "none";
+	  document.forms[0].style.display = "none";
+	  //Display confirmtation message
+	  var m = document.getElementById('message');
+	  m.style.color = "#824328";
+	  m.style.fontSize = "20px";
+	  m.innerHTML = "Table is reserved. <br>Reservation details are:<br> Date: " + d + "<br> Time: " + t + "<br> Party Size: " + ps.value + ".";
 	return false;
 	} 
-	
 } 
 
 
@@ -34,12 +94,7 @@ function ready()
 
 
 
- var today = new Date();
-	var tom = new Date(new Date().getTime() + 24*60*60*1000);
-	
-	 var d = document.getElementById('form-date');
-     d.valueAsDate = tom;
-   d.max = "2018-02-28";
+
 
 
 
