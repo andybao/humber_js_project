227bb1641aede8e10b4a6f9ca54c99d643f19c4d
raminder <raminderkaurmaan@yahoo.com>
2018-01-13 11:27:58 -0500

    hi

---
+++ js/about.js
@@ -0,0 +1,16 @@
+
+
+
+
+     $(document).ready(function(){
+		 $('.p2').css('display','none');
+		 $('.p3').css('display','none');
+		 
+    $("h2").click(function(){
+        $('.p2').fadeIn("slow");
+        $('.p3').fadeIn(3000);
+        //$("#div3").fadeIn(3000);
+    });
+});
+      
+
