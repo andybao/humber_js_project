508cbf720164333c1f554a0168657bb02f8b0045
bmw <abelgravesjr@gmail.com>
2018-01-14 03:34:42 -0500

    z

--- js/gift_cards.js
+++ js/gift_cards.js
@@ -1,30 +1,46 @@
+//
+// var amount = document.getElementsByName("name");
+//
+//
+// function setImage(amount) {
+//     switch (amount) {
+//     case 1:
+//         document.getElementById("imageShow").innerHTML = "<img src='images/10giftcard.png>";
+//         break;
+//     case 2:
+//         document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_30.png'>";
+//         break;
+//     case 3:
+//         document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_50.png'>";
+//         break;
+//     case 4:
+//         document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_75.png'>";
+//         break;
+//     case 5:
+//         document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_100.png'>";
+//         break;
+//     case 6:
+//         document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_150.png'>";
+//         break;
+//
+//         var yourSelection = document.getElementsByName('form');
+//         yourSelection[0].submit();
 
-var amount = document.getElementsByName("name");
 
 
-function setImage(amount) {
-    switch (amount) {
-    case 1:
-        document.getElementById("imageShow").innerHTML = "<img src='images/10giftcard.png>";
-        break;
-    case 2:
-        document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_30.png'>";
-        break;
-    case 3:
-        document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_50.png'>";
-        break;
-    case 4:
-        document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_75.png'>";
-        break;
-    case 5:
-        document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_100.png'>";
-        break;
-    case 6:
-        document.getElementById("imageShow").innerHTML = "<img src='images/giftcard_150.png'>";
-        break;
+function evalGroup()
+{
+var group = document.radioForm.myRadio;
+for (var i=0; i<group.length; i++) {
+if (group[i].checked)
+break;
+}
+if (i==group.length)
+return alert("No radio button is checked");
+alert("Radio Button " + (i+1) + " is checked.");
+}
 
-        var yourSelection = document.getElementsByName('form');
-        yourSelection[0].submit();
+
 
         // function yourSelection() {
         //   if (document.getElementsByName("name").value=="") {
